69550499f1a988ecf1b590a5e9a181d23b421aad
syseoz <syseoz@naver.com>
2023-05-31 22:15:12 +0900

    test docker

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/airbnb_client_deploy",
+  base: "./",
+  assetsInclude: ["**/*.jpg", "**/*.png"],
 });
